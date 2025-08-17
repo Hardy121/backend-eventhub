b239157dc70f0a77bdfbbc5e8d45e06909a00763
Hardy121 <merh0783@gmail.com>
2025-08-17 12:49:00 +0530

    Remove uploads error

--- src/controller/event.controller.js
+++ src/controller/event.controller.js
@@ -3,60 +3,60 @@ const Events = require("../models/event.schema");
 const { sendingDataInHeader } = require("../services/sendingDataInHeader");
 const { apiResponse } = require("../utils/apiResponse");
 
-async function createEvents(req, res) {
-    const userData = await sendingDataInHeader(req);
-    if (!userData) return apiResponse(res, 401, "Unauthorised request");
-    const { title, description, date, startTime, endTime, location, overView, goodToKnow } = req.body;
+    async function createEvents(req, res) {
+        const userData = await sendingDataInHeader(req);
+        if (!userData) return apiResponse(res, 401, "Unauthorised request");
+        const { title, description, date, startTime, endTime, location, overView, goodToKnow } = req.body;
 
-    let images;
-    if (req.files) {
-        images = await Promise.all(
-            req.files.map(async (file) => {
-                const url = await cloudinary.uploader.upload(file.path, {
-                    folder: 'event app'
-                });
-                return {
-                    public_id: url?.public_id,
-                    url: url?.secure_url
-                }
-            })
-        );
+        let images;
+        if (req.files) {
+            images = await Promise.all(
+                req.files.map(async (file) => {
+                    const url = await cloudinary.uploader.upload(file.path, {
+                        folder: 'event app'
+                    });
+                    return {
+                        public_id: url?.public_id,
+                        url: url?.secure_url
+                    }
+                })
+            );
+        }
+        if (
+            !title ||
+            !description ||
+            !date ||
+            !startTime ||
+            !endTime ||
+            !location) {
+            return apiResponse(res, 400, 'All fields are required', null)
+        }
+
+        if (date <= Date.now()) {
+            return apiResponse(res, 400, 'Event date must be in the future', null)
+        }
+
+        // if (eventTickets.length < 1) {
+        //     return apiResponse(res, 400, 'At least one ticket type is required', null)
+        // }
+
+        const data = await Events.create({
+            title,
+            description,
+            images,
+            date,
+            startTime,
+            endTime,
+            location,
+            overView,
+            goodToKnow,
+            organizer: userData?.id,
+            // eventTickets,
+            ispublished: 'draft'
+        });
+
+        return apiResponse(res, 200, "success", data)
     }
-    if (
-        !title ||
-        !description ||
-        !date ||
-        !startTime ||
-        !endTime ||
-        !location) {
-        return apiResponse(res, 400, 'All fields are required', null)
-    }
-
-    if (date <= Date.now()) {
-        return apiResponse(res, 400, 'Event date must be in the future', null)
-    }
-
-    // if (eventTickets.length < 1) {
-    //     return apiResponse(res, 400, 'At least one ticket type is required', null)
-    // }
-
-    const data = await Events.create({
-        title,
-        description,
-        images,
-        date,
-        startTime,
-        endTime,
-        location,
-        overView,
-        goodToKnow,
-        organizer: userData?.id,
-        // eventTickets,
-        ispublished: 'draft'
-    });
-
-    return apiResponse(res, 200, "success", data)
-}
 
 async function updateOrganisersEvents(req, res) {
     const userData = await sendingDataInHeader(req);
